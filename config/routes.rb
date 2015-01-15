74251c7ccfc5d5aaf7476744e1a50351251cecd3
Jim Withington <jamesrwithington@gmail.com>
2015-01-15 15:09:06 -0800

    Change route to use index instead of visitors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-
-  root to: 'visitors#index'
+ # Boostrap default root
+ # resources :users
+ # root to: 'visitors#index'
+ 
+ # in hartl they'd do
+ resources :users
+ root 'users#index'
 end
