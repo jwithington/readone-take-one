713236cdba05bd35e78c9bed62f853058a35b215
Jim Withington <jamesrwithington@gmail.com>
2015-01-16 17:30:25 -0800

    Finish basic user setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, notice: 'Welcome to ReadOne!' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -69,6 +69,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password, 
+                                  :password_confirmation)
     end
 end
