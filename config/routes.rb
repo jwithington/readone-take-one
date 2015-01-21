5f7ec3607ebd8bd8bb6dab6551f090c81f159e18
Jim Withington <jamesrwithington@gmail.com>
2015-01-21 12:36:21 -0800

    Refactor pages to simplify and eliminate access to other user info

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,32 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+ #  get 'static_pages/home'
 
-  get 'static_pages/help'
+ #  get 'static_pages/help'
 
-  get 'static_pages/about'
-  get 'sessions/new'
+ #  get 'static_pages/about'
+ #  get 'sessions/new'
 
   resources :feeds
 
- # Boostrap default root
- # resources :users
- # root to: 'visitors#index'
+ # # Boostrap default root
+ # # resources :users
+ # # root to: 'visitors#index'
  
- # in hartl they'd do
+ # # in hartl they'd do
  resources :users
- root 'sessions#new'
+ # root 'sessions#new'
  
- #from hartl chap 8 on logins
- get    'signup'  => 'users#new'
- get    'login'   => 'sessions#new'
- post   'login'   => 'sessions#create'
- delete 'logout'  => 'sessions#destroy'
+ # #from hartl chap 8 on logins
+ # get    'signup'  => 'users#new'
+ # get    'login'   => 'sessions#new'
+ # post   'login'   => 'sessions#create'
+ # delete 'logout'  => 'sessions#destroy'
+  root                'static_pages#home'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  # get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 end
