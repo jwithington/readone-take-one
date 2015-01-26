42561b163b022c8be13ad4c4582c53a21d4a844b
Jim Withington <jamesrwithington@gmail.com>
2015-01-26 14:07:12 -0800

    Log changes before database recreation

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,26 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# set up mailing
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+	:tls => true,
+	:address => "smtp.gmail.com",
+	:port => 587,
+	:domain => "gmail.com",
+	:authentication => :login,
+	user_name: ENV["GMAIL_USERNAME"],
+	password: ENV["GMAIL_PASSWORD"]
+}
+
+# alternately
+# config.action_mailer.smtp_settings = {
+#   address: "smtp.gmail.com",
+#   port: 587,
+#   domain: "example.com",
+#   authentication: "plain",
+#   enable_starttls_auto: true,
+#   user_name: ENV["GMAIL_USERNAME"],
+#   password: ENV["GMAIL_PASSWORD"]
+# }
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,8 @@ Rails.application.configure do
 
   # mailer setup for Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
   
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
